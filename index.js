79d608d574c839900bcb6cc574bf87fbf649644e
chengkungui <greg.gui@gmail.com>
2014-09-09 21:59:08 +1000

    init

---
+++ index.js
@@ -0,0 +1,11 @@
+'use strict';
+
+var express = require('express');
+
+var app = express();
+
+app.use(express.static('public'));
+
+var server = app.listen(3000, function(){
+    console.log('Listening to 3000')
+});
