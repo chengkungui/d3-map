6d5db748c17840f345f8107052231cd535ad9059
chengkungui <greg.gui@gmail.com>
2014-09-14 20:54:11 +1000

    add compression to the server

--- server.js
+++ server.js
@@ -1,10 +1,13 @@
 'use strict';
 
 var express = require('express');
+var compression = require('compression');
 
 var app = express();
 
 app.set('port', process.env.PORT || 3000);
+
+app.use(compression());
 
 app.use(express.static('public'));
 
